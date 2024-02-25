893fcf260ba195df27a2279ec7fefdcb2f04bd87
juligarc91 <julissagarcia@pursuit.org>
2024-02-25 14:07:11 -0500

    added route for Form

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Log from './Log';
 import Logs from './Logs';
+import LogForm from './LogForm';
 
 function App() {
   const [logs, setLogs] = useState([]); 
@@ -13,6 +14,7 @@ function App() {
           <Route path="/logs" element={<Logs logs={logs} setLogs={setLogs} setToggleDetails={setToggleDetails} />}/>
           <Route path="/logs/:id" element={toggleDetails.show && <Log toggleDetails={toggleDetails}/>}/>
           {/* maybe add route for form (logs/form) so it can be on different page instead of being on Logs view */}
+          <Route path="/logs/form" element={<LogForm setLogs={setLogs}/>}/>
         </Routes>
         {/* Instead of using Routes, use the next lines to show the selected item on same page */}
         {/* <Logs logs={logs} setLogs={setLogs} setToggleDetails={setToggleDetails} />
