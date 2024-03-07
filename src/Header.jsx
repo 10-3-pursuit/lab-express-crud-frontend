71fc846c1629ccc762f99fcaad6130ad2d05b993
juligarc91 <julissagarcia@pursuit.org>
2024-03-07 18:59:03 -0500

    added home button and link to logs from home page

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom"
+import "./App.css"
 
 const Header = () => {
   return (
     <header>
         <Link to="/logs">
           <h4>Daily Log Journal📓</h4>
+        </Link>
+        <Link to="/">
+          <div className="home-link">
+            <button >Home</button>
+          </div>
         </Link>
     </header>
   )
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
 const Home = () => {
   return (
-    <img style={{ display: 'block', margin: '0 auto' }} src="https://i.etsystatic.com/37307182/r/il/287958/4155769518/il_570xN.4155769518_5xwa.jpg" alt="journal"/>
+    <Link to="/logs">
+      <img style={{ display: 'block', margin: '0 auto' }} src="https://i.etsystatic.com/37307182/r/il/287958/4155769518/il_570xN.4155769518_5xwa.jpg" alt="journal"/>
+    </Link>
   )
 }
 
-export default Home
+export default Home;
